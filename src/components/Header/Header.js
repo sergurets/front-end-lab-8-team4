f4215ee51ba1525ca5b3dd6d24ba1c67e4929184
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-04-17 10:58:30 +0300

    Changed a to Link in Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './Header.css';
 
 class Header extends React.Component {
 	render() {
 		return (
-			<header>
-				<nav className="nav-header">
-			<a href="#job-container" className="nav-item-link">Find a job</a>
-						<a href="#" className="nav-item-link">Post a job</a>
-						<a href="#about-us" className="nav-item-link">About us</a>
-		</nav>
-		<div className = "div-header">
-			<a href="" className="nav-item-link">Sign in</a>
-			<a href="" className="div-header-img"></a>
-		</div>
+			<header className = "header">
+				<nav className="header__nav">
+					<a href="#job-container" className="header__nav__item">Find a job</a>
+					<Link to="#" className="header__nav__item">Post a job</Link>
+					<a href="#about-us" className="header__nav__item">About us</a>
+				</nav>
+				<div className = "header__div">
+					<Link to="" className="header__nav__item">Sign in</Link>
+					<Link to="" className="header__div__img"></Link>
+				</div>
 			</header>
 		);
 	}
