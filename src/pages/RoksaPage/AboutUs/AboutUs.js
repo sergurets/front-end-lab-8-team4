aff014b9aa0c7cbf45186139a47b4239b693a210
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-04-17 11:05:52 +0300

    Fixed css files according to BEM

--- src/pages/RoksaPage/AboutUs/AboutUs.js
+++ src/pages/RoksaPage/AboutUs/AboutUs.js
@@ -5,8 +5,8 @@ class AboutUs extends React.Component{
 	render(){
 		return (
 			<div id = "about-us">
-				<h1>About Us</h1>
-				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h1 className = "about-us__header">About Us</h1>
+				<p className = "about-us__text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		);
 	}
