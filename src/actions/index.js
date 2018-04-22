81eea7293a8c5db339bb912ae1415ece38c6181c
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-04-22 22:05:49 +0300

    Took away the comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,29 +9,3 @@ export function jobList(){
 			});
 		});
 	};
-	/*let data = '';
-	let job  => dispatch({
-		firebaseJobs.once('value').then((snapshot) => {
-		data = snapshot.val();
-		console.log(data);
-	});
-	}
-		job();
-	console.log(data);
-	return{
-		type : 'jobList',
-		jobs : [
-			{id:1, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'},
-			{id:2, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'},
-			{id:3, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'},
-			{id:4, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'},
-			{id:5, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'},
-			{id:6, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'},
-			{id:7, description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', job_title : 'Cleaning job', owner: 'df'}
-		]
-	}*/
-}
-/*firebaseJobs.once('value').then((snapshot) => {
-		data = snapshot.val();
-		console.log(data);
-	});*/
