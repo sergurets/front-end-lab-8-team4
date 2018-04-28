fbcb44ddd6f1d8f13fdd266d2bd9ecd10504d112
Sam Grytskiv <semen.grytskiv@gmail.com>
2018-04-28 04:26:41 +0300

    added user profile

--- src/pages/SamPage/UserInfo/UserInfo.js
+++ src/pages/SamPage/UserInfo/UserInfo.js
@@ -2,48 +2,45 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { userList } from '../../../actions';
 import FB from '../../../firebase';
+import UserJobList from '../UserJobList/UserJobList'
 
 class UserInfo extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.logOut = this.logOut.bind(this);
-
-		this.state = {
-			name: '',
-			email: '',
-			surname: '',
-			city: ''
-		}
+		this.userEmail = ''
 	}
 
 	componentWillMount() {
 		this.props.getUsers();
 	}
 
-	setStateWithFoundEmail() {
-		let email = FB.firebase.auth().currentUser.email;
+	renderUsersInfo = (obj) => {
+		// let email = FB.firebase.auth().currentUser.email;
+		let email = 'sef@rhtt.er';
 
-		//convert store from obj to arr
-		let users = [];
-		for (var key in this.props.data) {
-			users.push(this.props.data[key]);
+		this.userEmail = email;
+
+		console.log('call');
+		var temp = {};
+		for (var key in obj) {
+			if (obj[key].email === email) {
+				temp = Object.assign({}, obj[key]);
+			}
 		}
 
-		//search for user by email
-		let user = users.find(el => {
-			return el.email === email;
-		});
-
-		console.log(user.name);
-
-		// setting values
-		this.setState({
-			name: user.name,
-			email: user.email,
-			surname: user.surname,
-			city: user.city
-		});
+		return (
+			<div>
+				<ul className="">
+					<li>Name: {temp.name}</li>
+					<li>Surname: {temp.surname}</li>
+					<li>City: {temp.city}</li>
+					<li>Email: {temp.email}</li>
+					<li>Photo: <img src={temp.url}></img></li>
+				</ul>
+			</div>
+		)
 
 	}
 
@@ -56,34 +53,34 @@ class UserInfo extends React.Component {
 	}
 
 
-	// renderList = (obj) => {
-	// 	let users = [];
-	// 	for (var key in obj) {
-	// 		users.push(obj[key]);
-	// 	}
+	renderList = (obj) => {
+		let users = [];
+		for (var key in obj) {
+			users.push(obj[key]);
+		}
 
-	// 	return (
-	// 		users.map(item => (
-	// 			<li className="" key={item.id}>
-	// 				<h3>{item.name}</h3>
-	// 				<p>{item.surname}</p>
-	// 			</li>
-	// 		))
-	// 	);
-	// }
+		return (
+			users.map(item => (
+				<li className="" key={item.id}>
+					<h3>{item.name}</h3>
+					<p>{item.surname}</p>
+				</li>
+			))
+		);
+	}
 
 	render() {
-		this.setStateWithFoundEmail();
 		return (
 			<div id="">
 				<h1>Users Info</h1>
 				<button onClick={this.logOut}>Log Out</button>
-				<ul className="">
-					<li>Name: {this.state.name}</li>
-					<li>Surname: {this.state.surname}</li>
-					<li>City: {this.state.city}</li>
-					<li>Email: {this.state.email}</li>
-				</ul>
+				<ol>
+					{/* {this.renderList(this.props.data)} */}
+				</ol>
+				<div>
+					{this.renderUsersInfo(this.props.data)}
+					<UserJobList email={this.userEmail} />
+				</div>
 			</div>
 		);
 	}
---
+++ src/pages/SamPage/UserJobList/UserJobList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { jobList } from '../../../actions';
+import { Link } from "react-router-dom";
+
+class JobList extends React.Component {
+    componentWillMount() {
+        this.props.getJobs()
+    }
+    renderList = (jobList) => {
+        let jobs = [];
+        for (var key in jobList) {
+            jobs.push(jobList[key]);
+        }
+        return (
+            jobs ?
+                jobs.filter(el => {
+                    // el.email
+                }).map(item => (
+                    <li className="job-list__item" key={item.id}>
+                        <Link to={`/jobInfo/#${item.id}`}>{item.title}</Link>
+                        <p>{item.info}</p>
+                    </li>
+                )) : null
+        );
+    }
+
+    render() {
+        console.log(this.props.data.jobList);
+        return (
+            <div id="job-container">
+                <h1> Jobs</h1>
+                <ol className="job-list">
+                    {this.renderList(this.props.data.jobList)}
+                </ol>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        data: state.jobList
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getJobs: () => {
+            dispatch(jobList())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JobList);
