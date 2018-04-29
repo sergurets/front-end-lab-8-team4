c5e2b5e9ec9dc6989a4c3e9728fc5cd936e3749c
sergurets <sergurets@gmail.com>
2018-04-29 23:10:49 +0300

    add_map/location_to_job_

---
+++ src/pages/SergiyPage/Map/map.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './map.css';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+import { connect } from 'react-redux';
+import { jobList } from '../../../actions';
+import { Link } from "react-router-dom";
+
+class WorkMap extends React.Component{
+	
+
+	componentWillMount(){
+		console.log(this.props.name)
+		this.props.getJobs()
+	}
+	renderWorksOnMap = () =>{
+
+		return(
+
+					<Marker position={{ lat: Number(`${this.props.name.lat}`), lng: Number(`${this.props.name.lng}`) }}/>
+
+		);
+	}
+	handleMarkerClick = (props, marker, e) => {
+		console.log(e);
+  	}
+
+   render(){
+this.props.name
+   const WorksMap = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter = { { lat: this.props.name.lat, lng: this.props.name.lng } }
+        defaultZoom = { 12 }
+      >
+      {this.renderWorksOnMap(this.props.data.jobList)}
+      </GoogleMap>
+   ));
+   return(
+      <div >
+	
+        <WorksMap
+          containerElement={ <div id = "work-map"/> }
+          mapElement={ <div style={{ height: `100%` }} /> }
+        />
+      </div>
+   );
+   }
+};
+
+const mapStateToProps = (state) => {
+	return {
+		data: state.jobList
+	}
+}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getJobs : () => {
+			dispatch(jobList())
+		}
+	}
+}
+ export default connect(mapStateToProps,mapDispatchToProps)(WorkMap) 
--- src/pages/TaniaPage/Login.js
+++ src/pages/TaniaPage/Login.js
@@ -6,10 +6,10 @@ import './Login.css';
 class Login extends Component{
 	constructor(props){
 	    super(props);
-		    this.state = {
-		      password: "",
-		      email: ""
-		};
+		  this.state = {
+		    password: "",
+		    email: ""
+		  };
 	    this.handleChange = this.handleChange.bind(this);
 	    this.handleSubmit = this.handleSubmit.bind(this);
 	    this.singOut = this.singOut.bind(this);
@@ -49,7 +49,7 @@ class Login extends Component{
 			        placeholder="E-mail"
 			        name="email"
 			        ref="email"
-			        className="regform__input"
+			        className="regform__email"
 			        value={this.state.email}
         			onChange={this.handleChange}
 			        required
@@ -58,7 +58,7 @@ class Login extends Component{
 			        placeholder="Enter password"
 			        name="password"
 			        ref="pass"
-			        className="regform__input"
+			        className="regform__pass"
 			        value={this.state.password}
         			onChange={this.handleChange}
 			        required
