8dcec8bc06e97b97d2918a0e53f649046c674e50
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-04-29 23:39:13 +0300

    Tooke away console log in Marker

--- src/pages/RoksaPage/Map/map.js
+++ src/pages/RoksaPage/Map/map.js
@@ -21,7 +21,6 @@ class WorkMap extends React.Component{
 			jobs.map(item =>(
 					<Marker key={Number(`${item.id}`)} position={{ lat: Number(`${item.lat}`), lng: Number(`${item.lng}`) }}
 						onClick= {(e) => {
-							console.log(e.latLng.lng())
 							window.location.href =`/jobInfo/#${item.id}`;
 						}}>
 						</Marker>
