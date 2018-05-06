02bed92216a37772de266f68d927ba0181ee0664
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-05-06 14:37:02 +0300

    Added styles to header and aboutUs section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './Header.css';
+import $ from "jquery";
 
 class Header extends React.Component {
+	onScroll = () =>{
+		$(window).scroll(function() {
+		    var scroll = $(window).scrollTop();
+		    if (scroll >= 50) {
+		        $(".header").addClass("header--scrolled");
+		    }else{
+		    	$(".header").removeClass("header--scrolled");
+		    }
+		});
+	}
 	render() {
+		this.onScroll();
 		return (
-			<header className="header">
+			<header className="header section--header ">
 				<nav className="header__nav">
-					<a href="/Page#job-container" className="header__nav__item">Find a job</a>
-					<Link to="/Addjob" className="header__nav__item">Post a job</Link>
-					<a href="/Page#about-us" className="header__nav__item">About us</a>
+					<a href="/Page#job-container" className="header__nav-links">Find a job</a>
+					<Link to="/Addjob" className="header__nav-links">Post a job</Link>
+					<a href="/Page#about-us" className="header__nav-links">About us</a>
 
 				</nav>
 				<div className="header__div">
-					<Link to="/Login" className="header__nav__item">Sign in</Link>
+					<Link to="/Login" className="header__nav-links">Sign in</Link>
 					<Link to="/UserProfile" className="header__div__img"></Link>
 				</div>
 			</header>
