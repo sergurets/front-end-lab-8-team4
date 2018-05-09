4f8b30b7aeba12014f7c1e9f687594f6613c0ac2
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-05-09 09:26:55 +0300

    add redirect

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
+import fireb from './firebase'
 
 const createStoreWidthMiddleWare = applyMiddleware(thunk)(createStore);
-
-ReactDOM.render(
-	<Provider store={createStoreWidthMiddleWare(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-);
-
+let store = createStoreWidthMiddleWare(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+fireb.firebase.auth().onAuthStateChanged((user)=>{
+	ReactDOM.render(
+		<Provider store={store}>
+			<App auth={user}/>
+		</Provider>,
+		document.getElementById('root')
+	);
+});
