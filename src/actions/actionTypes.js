e8841f4c0b91ca7c32cca60165f5694dadbc55fc
Sam Grytskiv <semen.grytskiv@gmail.com>
2018-05-10 11:05:36 +0300

    created actionTypes folder, added types of actions

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export const FETCH_JOB_LIST = 'FETCH_JOB_LIST';
+export const FETCH_USER_LIST = 'FETCH_USER_LIST';
+export const GET_USER = 'GET_USER';
+export const ADD_JOB = 'ADD_JOB';
--- src/reducers/userList_reducer.js
+++ src/reducers/userList_reducer.js
@@ -1,6 +1,8 @@
+import { FETCH_USER_LIST } from '../actions/actionTypes';
+
 export default function (state = {}, action) {
 	switch (action.type) {
-		case 'FETCH_USER_LIST':
+		case FETCH_USER_LIST:
 			return { ...state, users: action.payload };
 		default:
 			return state;
