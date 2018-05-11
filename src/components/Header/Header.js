1d8e8a46d1b0374a463ba19ba37ceb0df0fe3113
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-05-11 22:33:52 +0300

    Deleted comments from header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,13 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header className="header">
-				<nav className="header__nav">//header-nav
+				<nav className="header__nav">
 					<a href="/Page#job-container" className="header__nav__item">Find a job</a>
 					<Link to="/Addjob" className="header__nav__item">Post a job</Link>
 					<a href="/Page#about-us" className="header__nav__item">About us</a>
 
 				</nav>
-				<div className="header__div">//header-div
+				<div className="header__div">
 					<Link to="/Login" className="header__nav__item">Sign in</Link>
 					<Link to="/UserProfile" className="header__div__img"></Link>
 				</div>
