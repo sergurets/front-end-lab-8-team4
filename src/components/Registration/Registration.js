4614c2fe05f1815cc87d1f10419cc9b039299fe5
Tanya Bilanyuk <34652839+spookyTanya@users.noreply.github.com>
2018-05-17 12:54:57 +0300

    Update Registration.js

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -20,57 +20,62 @@ class App extends Component {
 
 	handleSubmit(event) {
 		event.preventDefault();
-		let data = {
-			name: this.state.name,
-			email: this.state.email.toLowerCase(),
-			password: this.state.password,
-			city: this.state.city,
-			surname: this.state.surname,
-		}
-		if (this.state.image) {
-			data.image = this.state.image;
-		}
-		fireb.firebaseTrueUsers.orderByChild('email').equalTo(data.email).once("value", function (snapshot) {
-			if (!snapshot.val()) {
-				let key = fireb.firebaseTrueUsers.push(data).key;
-				let Ref = firebase.database().ref(`usersT/${key}`);
-				Ref.update({
-					"databaseId": key
-				});
+    if(this.state.password.length <= 6){
+      alert('Too short password!');
+    } else {
+  		let data = {
+  			name: this.state.name,
+  			email: this.state.email.toLowerCase(),
+  			password: this.state.password,
+  			city: this.state.city,
+  			surname: this.state.surname,
+  		}
+  		if (this.state.image) {
+  			data.image = this.state.image;
+  		}
+  		fireb.firebaseTrueUsers.orderByChild('email').equalTo(data.email).once("value", function (snapshot) {
+  			if (!snapshot.val()) {
+  				let key = fireb.firebaseTrueUsers.push(data).key;
+  				let Ref = firebase.database().ref(`usersT/${key}`);
+  				Ref.update({
+  					"databaseId": key
+  				});
 
-				if (data.image) {
-					let filename = data.image.name;
-					let storageRef = fireb.firebase.storage().ref('/userImages/' + filename);
+  				if (data.image) {
+  					let filename = data.image.name;
+  					let storageRef = fireb.firebase.storage().ref('/userImages/' + filename);
 
-					let upload = storageRef.put(data.image);
-					upload.on('state_changed', function (snapshot) {
+  					let upload = storageRef.put(data.image);
+  					upload.on('state_changed', function (snapshot) {
 
-					}, function (error) {
-					}, function () {
-						let downloadURL = upload.snapshot.downloadURL;
-						let updates = {};
-						data.url = downloadURL;
-						updates['/usersT/' + key] = data;
-						fireb.firebaseDB.ref().update(updates);
-					});
-				} else {
-					let updates = {};
-					data.url = "https://static.change.org/profile-img/default-user-profile.svg";
-					updates['/usersT/' + key] = data;
-					fireb.firebaseDB.ref().update(updates);
-				}
-				fireb.firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
-					.then(res => {
-						alert("Successfully registrated");
-					})
-					.catch(error => {
-						alert(error);
-					});
-			}
-		});
-		for (let key in this.state) {
-			this.setState({ [key]: "" })
-		}
+  					}, function (error) {
+  					}, function () {
+  						let downloadURL = upload.snapshot.downloadURL;
+  						let updates = {};
+  						data.url = downloadURL;
+  						updates['/usersT/' + key] = data;
+  						fireb.firebaseDB.ref().update(updates);
+  					});
+  				} else {
+  					let updates = {};
+  					data.url = "https://static.change.org/profile-img/default-user-profile.svg";
+  					updates['/usersT/' + key] = data;
+  					fireb.firebaseDB.ref().update(updates);
+  				}
+  				fireb.firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+  					.then(res => {
+  						alert("Successfully registrated");
+              
+  					})
+  					.catch(error => {
+  						alert(error);
+  					});
+  			}
+  		});
+  		for (let key in this.state) {
+  			this.setState({ [key]: "" })
+  		}
+    	}
 	}
 
 	handleChange(event) {
